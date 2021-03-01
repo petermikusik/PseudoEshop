5c5dd3716bae2456af916ec37c70135c2fc1001f
Mikusik <mikusik.p@gmail.com>
2021-03-01 22:34:15 +0100

    Implemented getAllProducts and getProductById

--- src/main/java/com/example/Shopv2/controller/ProductController.java
+++ src/main/java/com/example/Shopv2/controller/ProductController.java
@@ -1,9 +1,30 @@
 package com.example.Shopv2.controller;
 
+import com.example.Shopv2.model.Product;
+import com.example.Shopv2.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id){
+        return productService.getProductById(id);
+    }
 }
--- src/main/java/com/example/Shopv2/service/ProductService.java
+++ src/main/java/com/example/Shopv2/service/ProductService.java
@@ -1,7 +1,31 @@
 package com.example.Shopv2.service;
 
+import com.example.Shopv2.model.Product;
+import com.example.Shopv2.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        Optional<Product> productOptional = productRepository.findById(id);
+        if (productOptional.isPresent()){
+            return productOptional.get();
+        } else {
+            throw new IllegalStateException("Product with id " + id + " does not exists");
+        }
+    }
 }
