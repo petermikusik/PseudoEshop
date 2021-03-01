a5db4d5cfbd31de132e8957cbdbcbf2c9f83e606
Mikusik <mikusik.p@gmail.com>
2021-03-01 22:27:16 +0100

    Added Product Repository, Service and RestController

---
+++ src/main/java/com/example/Shopv2/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.example.Shopv2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+}
