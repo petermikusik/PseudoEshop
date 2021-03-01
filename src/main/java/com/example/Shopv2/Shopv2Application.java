cae17773b5d29a398049d512796d762a564d1bbd
Mikusik <mikusik.p@gmail.com>
2021-03-01 19:47:33 +0100

    First commit after project creation

---
+++ src/main/java/com/example/Shopv2/Shopv2Application.java
@@ -0,0 +1,13 @@
+package com.example.Shopv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shopv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shopv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Shopv2/Shopv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shopv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shopv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
