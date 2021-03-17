ea0622f140247d5d7789cb4b19b0bf3c6d497411
Mikusik <mikusik.p@gmail.com>
2021-03-17 14:12:26 +0100

    For every new Customer there is a new Account automatically

--- src/main/java/com/example/Shopv2/service/CustomerService.java
+++ src/main/java/com/example/Shopv2/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.example.Shopv2.service;
 
 import com.example.Shopv2.model.Customer;
+import com.example.Shopv2.model.CustomerAccount;
 import com.example.Shopv2.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,11 @@ import java.util.Optional;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final CustomerAccountService customerAccountService;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerAccountService customerAccountService) {
         this.customerRepository = customerRepository;
+        this.customerAccountService = customerAccountService;
     }
 
     public List<Customer> getAllCustomers(){
@@ -35,5 +38,9 @@ public class CustomerService {
             throw new IllegalStateException("Customer with email " + customer.getEmail() + " already exists");
         }
         customerRepository.save(customer);
+        CustomerAccount customerAccount = new CustomerAccount();
+        customerAccount.setCustomer(customer);
+        customerAccount.setMoney(0);
+        customerAccountService.addCustomerAccount(customerAccount);
     }
 }
